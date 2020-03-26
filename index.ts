8f920211d43f81152ba4210f21e0859323a975a8
leeyunhome <leeyunhome@daum.net>
2020-03-26 14:00:31 +0900

    #2 Setting Typescript Up

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "leehyerim",
+    age = 24,
+    gender = "female";
+
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+export {};
