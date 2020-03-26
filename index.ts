57bff1d88c36d8cbca0b5ae4e963c990e97ac9b4
leeyunhome <leeyunhome@daum.net>
2020-03-26 14:05:50 +0900

    #3 simple typescript function

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ const name = "leehyerim",
     age = 24,
     gender = "female";
 
-const sayHi = (name, age, gender) => {
+const sayHi = (name, age, gender?) => {
     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
 
-sayHi(name, age, gender);
+sayHi(name, age);
 
 export {};
