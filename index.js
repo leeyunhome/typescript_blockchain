09f92d011894648811f36cc247371ac70cbbf8ce
leeyunhome <leeyunhome@daum.net>
2020-03-26 15:21:46 +0900

    #6 classe

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 "use strict";
 exports.__esModule = true;
-var sayHi = function (name, age, gender) {
-    return "Hello " + name + ", you are " + age + ", you are a " + gender;
+// interface Human {
+//     name: string;
+//     age: number;
+//     gender:string;
+// }
+var Human = /** @class */ (function () {
+    function Human(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    return Human;
+}());
+var leehyerim = new Human("hyerim", 32, "female");
+// const person = {
+//     name: "leehyerim",
+//     age: 32,
+//     gender: "female",
+// }
+var sayHi = function (person) {
+    return "Hello " + person.name + ", you are " + person.age + ", you are a " + person.gender;
 };
-console.log(sayHi("YLeehyerim", 32, "female"));
+console.log(sayHi(leehyerim));
--- index.ts
+++ index.ts
@@ -3,17 +3,28 @@
 //     age: number;
 //     gender:string;
 // }
-
-const person = {
-    name: "leehyerim",
-    age: 32,
-    gender: "female",
-
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
+
+const leehyerim = new Human("hyerim", 32, "female");
+// const person = {
+//     name: "leehyerim",
+//     age: 32,
+//     gender: "female",
+
+// }
 const sayHi = (person): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(leehyerim));
 
 export {};
