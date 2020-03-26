138d6d79a54a391ae51a4944842c9349e714149f
leeyunhome <leeyunhome@daum.net>
2020-03-26 15:38:15 +0900

    #7 Blockchain Creating a Block

--- index.ts
+++ index.ts
@@ -1,30 +1,29 @@
-// interface Human {
-//     name: string;
-//     age: number;
-//     gender:string;
-// }
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index:number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number,
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const leehyerim = new Human("hyerim", 32, "female");
-// const person = {
-//     name: "leehyerim",
-//     age: 32,
-//     gender: "female",
+const genesisBlock:Block = new Block(0, "2020202020", "", "Hello", 123456);
 
-// }
-const sayHi = (person): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(leehyerim));
+
+console.log(blockchain);
 
 export {};
